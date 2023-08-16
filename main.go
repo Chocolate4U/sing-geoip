3b5d74a94b9c6f15a1361458fa7c02b612e89cfb
chocolate4u <133856779+chocolate4u@users.noreply.github.com>
2023-08-16 15:28:36 +0330

    some modifications

--- main.go
+++ main.go
@@ -11,9 +11,8 @@ import (
 
 	"github.com/sagernet/sing/common"
 	E "github.com/sagernet/sing/common/exceptions"
-	"github.com/sagernet/sing/common/rw"
 
-	"github.com/google/go-github/v45/github"
+	"github.com/google/go-github/v53/github"
 	"github.com/maxmind/mmdbwriter"
 	"github.com/maxmind/mmdbwriter/inserter"
 	"github.com/maxmind/mmdbwriter/mmdbtype"
@@ -110,24 +109,6 @@ func newWriter(metadata maxminddb.Metadata, codes []string) (*mmdbwriter.Tree, e
 	})
 }
 
-func open(path string, codes []string) (*mmdbwriter.Tree, error) {
-	reader, err := maxminddb.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	if reader.Metadata.DatabaseType != "sing-geoip" {
-		return nil, E.New("invalid sing-geoip database")
-	}
-	reader.Close()
-
-	return mmdbwriter.Load(path, mmdbwriter.Options{
-		Languages: append(reader.Metadata.Languages, common.Filter(codes, func(it string) bool {
-			return !common.Contains(reader.Metadata.Languages, it)
-		})...),
-		Inserter: inserter.ReplaceWith,
-	})
-}
-
 func write(writer *mmdbwriter.Tree, dataMap map[string][]*net.IPNet, output string, codes []string) error {
 	if len(codes) == 0 {
 		codes = make([]string, 0, len(dataMap))
@@ -160,41 +141,10 @@ func write(writer *mmdbwriter.Tree, dataMap map[string][]*net.IPNet, output stri
 	return err
 }
 
-func local(input string, output string, codes []string) error {
-	binary, err := os.ReadFile(input)
-	if err != nil {
-		return err
-	}
-	metadata, countryMap, err := parse(binary)
-	if err != nil {
-		return err
-	}
-	var writer *mmdbwriter.Tree
-	if rw.FileExists(output) {
-		writer, err = open(output, codes)
-	} else {
-		writer, err = newWriter(metadata, codes)
-	}
-	if err != nil {
-		return err
-	}
-	return write(writer, countryMap, output, codes)
-}
-
-func release(source string, destination string) error {
+func release(source string, output string) error {
 	sourceRelease, err := fetch(source)
 	if err != nil {
 		return err
-	}
-	destinationRelease, err := fetch(destination)
-	if err != nil {
-		logrus.Warn("missing destination latest release")
-	} else {
-		if os.Getenv("NO_SKIP") != "true" && strings.Contains(*destinationRelease.Name, *sourceRelease.Name) {
-			logrus.Info("already latest")
-			setActionOutput("skip", "true")
-			return nil
-		}
 	}
 	binary, err := download(sourceRelease)
 	if err != nil {
@@ -212,36 +162,18 @@ func release(source string, destination string) error {
 	if err != nil {
 		return err
 	}
-	err = write(writer, countryMap, "geoip.db", nil)
-	if err != nil {
-		return err
-	}
-	writer, err = newWriter(metadata, []string{"cn"})
-	if err != nil {
-		return err
-	}
-	err = write(writer, countryMap, "geoip-cn.db", []string{"cn"})
+	err = write(writer, countryMap, output, nil)
 	if err != nil {
 		return err
 	}
 	if err != nil {
 		return err
 	}
-	setActionOutput("tag", *sourceRelease.Name)
 	return nil
 }
 
-func setActionOutput(name string, content string) {
-	os.Stdout.WriteString("::set-output name=" + name + "::" + content + "\n")
-}
-
 func main() {
-	var err error
-	if len(os.Args) >= 3 {
-		err = local(os.Args[1], os.Args[2], os.Args[3:])
-	} else {
-		err = release("Dreamacro/maxmind-geoip", "sagernet/sing-geoip")
-	}
+	err := release("Chocolate4U/Iran-v2ray-rules", "geoip.db")
 	if err != nil {
 		logrus.Fatal(err)
 	}
